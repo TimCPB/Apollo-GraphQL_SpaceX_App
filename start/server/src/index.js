d04a04b6c6834a8025c1b40689d1eac5e144f0d1
Tim <timcpb@hotmail.com>
2021-02-16 01:08:25 +0000

    create LaunchAPI RESTDataSource in launch.js and pass to ApolloServer in index.js

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,6 +1,14 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/dev
+  `);
+});
